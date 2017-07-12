ceae834cc72161b9270f07909a5546a6e0a0a3a0
huguobo <huguobo@meituan.com>
2017-07-12 16:22:36 +0800

    delete origin code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,5 @@
 import React, { Component } from 'react';
-//import logo from './logo.svg';
-//import './App.css';
 
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <div className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <h2>Welcome to React</h2>
-//         </div>
-//         <p className="App-intro">
-//           To get started, edit <code>src/App.js</code> and save to reload.
-//         </p>
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
 
 class ToDoList extends Component{
       constructor(props){
@@ -59,7 +40,6 @@ class TodoNew extends Component{
         this.setState({inputText:e.target.value});  
       }
      render () {
-
         return (
             <div>
                 <input type="text" onChange={this.changeInput.bind(this)}/>
@@ -77,6 +57,7 @@ class ListTodo extends Component{
       {item.toString()}
       </li>
       );
+
       return (
        <ul>{listItems}</ul>
       );

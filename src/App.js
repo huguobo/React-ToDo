f74a5f2c172ef306725767c46e22b6a091766c50
huguobo <huguobo@meituan.com>
2017-07-13 11:43:34 +0800

    add done and doing state feature

--- src/App.js
+++ src/App.js
@@ -1,86 +1,99 @@
 import React, { Component } from 'react';
 
-
 class ToDoList extends Component{
-      constructor(props){
-        super(props);
-        this.state={list:[]};
-        this.HandleChange=this.HandleChange.bind(this);
-        this.HandleDelete=this.HandleDelete.bind(this);
-      }
+  constructor(props){
+    super(props);
+    this.state={list:[]};
+    this.HandleChange=this.HandleChange.bind(this);
+    this.HandleDelete=this.HandleDelete.bind(this);
+  }
 
-      HandleChange(todo){
-        var rows=this.state.list;
-        rows.push(todo);
-        this.setState(
-            {list:rows}
-        );
-      }
-      
-      HandleDelete(index){
-          this.state.list.splice(index, 1);
-          this.setState({lists: this.state.lists});
-      }
-      
-      render(){
-        return (
-            <div>
-                 <TodoNew onAdd={this.HandleChange} />
-                 <ListTodo todo={this.state.list} deleteList={this.HandleDelete} />
-            </div>
-          );
-      }
-      
+  HandleChange(todo){
+    var rows=this.state.list;
+    rows.push(todo);
+    this.setState(
+      {list:rows}
+      );
+  }
+
+  HandleDelete(index){
+    this.state.list.splice(index, 1);
+    this.setState({lists: this.state.lists});
+  }
+
+  render(){
+    return (
+      <div>
+      <TodoNew onAdd={this.HandleChange} />
+      <ListTodo todo={this.state.list} deleteList={this.HandleDelete} />
+      </div>
+      );
+  }
+
 };
 
 class TodoNew extends Component{
-      constructor(props){
-        super(props);
-        this.state={inputText:''};
-      }
-      addTodo(e){
-        if(this.state.inputText){
-        this.props.onAdd(this.state.inputText);
-        }
-      }
-      changeInput(e){
-        this.setState({inputText:e.target.value});  
-      }
-     render () {
-        return (
-            <div>
-                <input type="text" onChange={this.changeInput.bind(this)}/>
-                <button onClick={this.addTodo.bind(this)} >添加事项</button>
-            </div>
-        );
+  constructor(props){
+    super(props);
+    this.state={inputText:''};
+  }
+  addTodo(e){
+    if(this.state.inputText){
+      this.props.onAdd(this.state.inputText);
     }
+  }
+  changeInput(e){
+    this.setState({inputText:e.target.value});  
+  }
+  render () {
+    return (
+      <div>
+      <input type="text" onChange={this.changeInput.bind(this)}/>
+      <button onClick={this.addTodo.bind(this)} >添加事项</button>
+      </div>
+      );
+  }
 }
 
-class ListTodo extends Component{
-    
-    constructor(props){
-      super(props);
-      this.handleDelete=this.handleDelete.bind(this);
-    }
-    handleDelete(e){
-           var index=e.target.getAttribute("id")
-           this.props.deleteList(index);
-    }
-    render(){
-      var  list=this.props.todo;
-
-      const listItems = list.map((item,index) =>
-      
+class ListTodo extends Component{   
+  constructor(props){
+    super(props);
+    this.handleDelete=this.handleDelete.bind(this);
+  }
+  handleDelete(e){
+    const index=e.target.getAttribute("id")
+    this.props.deleteList(index);
+  }
+  render(){
+    const list=this.props.todo;      
+    const listItems = list.map((item,index) =>
       <li key={index.toString()}>
-      {item.toString()}{"   "}<button id={index.toString()} onClick={this.handleDelete}>删除</button>
-      </li> 
-      
+      {item.toString()}{"   "}
+      <FinishTodo /> {"   "} 
+      <button id={index.toString()} onClick={this.handleDelete}>删除</button>    
+      </li>
       );
+    return (
+     <ul>{listItems} </ul>
+     );
+  }
+}
 
-      return (
-       <ul>{listItems}</ul>
+class FinishTodo extends Component{
+  constructor(props){
+    super(props);
+    this.hanldeFinish=this.hanldeFinish.bind(this);
+    this.state={isfinish:false};
+  }
+  hanldeFinish(){
+    this.setState({isfinish:!this.state.isfinish});
+  }
+  render(){
+    const isfinish=this.state.isfinish;
+    return (
+      <span><input type="checkbox" checked={isfinish} onClick={this.hanldeFinish} /> {isfinish?(<span>Done</span>):(<span>Doing</span>)}</span>
       );
-    }
+  }
 }
 
 
